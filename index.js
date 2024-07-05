24982ea01e559bdab7b3727ab66d0a0d0a36a0f4
Zidni Al 'Azmi <143054285+ZidniGz@users.noreply.github.com>
2024-07-05 10:07:45 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express")
 var app = express()
 const path = require("path")
+const got = require("got")
 
 app.use(express.static(path.join(__dirname, 'public')))
 
@@ -9,4 +10,5 @@ app.set("views", path.join(__dirname, "views"))
 
 app.get("/", (req,res)=>res.render("index"))
 app.get("/profile", (req, res)=>res.render("profile"))
+app.get("/views",(req,res)=>got('https://api.counterapi.dev/v1/azmi/azmi').json().then({count}=> res.send(count)))
 app.listen(3000)
