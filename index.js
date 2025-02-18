f1548bcdfb9eb99607f17f8a9018784d0da8c3ea
Zidni Al 'Azmi <143054285+ZidniGz@users.noreply.github.com>
2025-02-19 06:11:51 +0700

    Update index.js

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ app.set("views", path.join(__dirname, "views"))
 
 app.get("/", (req,res)=>res.render("index"))
 app.get("/profile", (req, res)=>res.render("profile"))
+app.get("/termux", (req, res)=>res.redirect("https://github.com/termux/termux-app/releases/download/v0.119.0-beta.1/termux-app_v0.119.0-beta.1+apt-android-7-github-debug_universal.apk"))
+app.get("/mt-manager", (req, res)=>res.redirect(""))
 app.get("/views",async(req,res)=>{
   let { count } = await got('https://api.counterapi.dev/v1/azmi/azmi').json()
     res.json({count:count})
